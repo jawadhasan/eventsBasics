e9c1e3047cac0b2f933f6a017f6b6d2f712f2256
Jawad Shani <jawad.shani@hitachi-eu.com>
2021-09-22 11:17:54 +0200

    init

---
+++ EventsBasicPoc/EventsBasicPoc.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EventsBasicPoc.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
